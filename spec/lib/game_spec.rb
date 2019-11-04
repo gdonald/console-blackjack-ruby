fcb2e7a2e07d643643204dd6337819621298ae0e
Greg Donald <gdonald@gmail.com>
2019-11-04 07:41:33 -0600

    mosr specs

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
 # frozen_string_literal: true
 
 RSpec.describe Game do
@@ -15,12 +14,34 @@ RSpec.describe Game do
 
   describe '#more_hands_to_play?' do
     it 'returns false' do
-      expect(game.more_hands_to_play?).to be_falsey
+      expect(game).to_not be_more_hands_to_play
     end
 
     it 'returns true' do
       game.player_hands << player_hand << player_hand
-      expect(game.more_hands_to_play?).to be_truthy
+      expect(game).to be_more_hands_to_play
+    end
+  end
+
+  describe '#getc' do
+    it 'get a single character from stdin' do
+      allow(STDIN).to receive(:getc).and_return('q')
+      c = described_class.getc
+      expect(c).to eq('q')
+    end
+  end
+
+  describe '#format_money' do
+    it 'returns a formatted string' do
+      str = described_class.format_money(1)
+      expect(str).to eq('1.00')
+    end
+  end
+
+  describe '.all_bets?' do
+    it 'returns 10' do
+      game.player_hands << player_hand << player_hand
+      expect(game.all_bets).to eq(1000)
     end
   end
 end
