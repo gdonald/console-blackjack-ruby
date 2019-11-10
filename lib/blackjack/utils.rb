9ec82759217c41005746171782e73e52cf7c395a
Greg Donald <gdonald@gmail.com>
2019-11-10 08:40:03 -0600

    cleanup

--- lib/blackjack/utils.rb
+++ lib/blackjack/utils.rb
@@ -16,28 +16,34 @@ module Utils
     self.current_bet = a[2].to_i
   end
 
-  def clear_draw_hands_new_num_decks
+  def clear_draw_hands
     clear
     draw_hands
+  end
+
+  def clear_draw_hands_new_num_decks
+    clear_draw_hands
     new_num_decks
   end
 
   def clear_draw_hands_new_deck_type
-    clear
-    draw_hands
+    clear_draw_hands
     new_deck_type
   end
 
   def clear_draw_hands_ask_insurance
-    clear
-    draw_hands
+    clear_draw_hands
     ask_insurance
   end
 
-  def play_dealer_hand
-    dealer_hand.play
-    draw_hands
+  def clear_draw_hands_bet_options
+    clear_draw_hands
     draw_bet_options
+  end
+
+  def clear_draw_hands_game_options
+    clear_draw_hands
+    draw_game_options
   end
 
   def draw_hands_current_hand_action
@@ -45,15 +51,9 @@ module Utils
     current_player_hand.action?
   end
 
-  def clear_draw_hands_bet_options
-    clear
+  def play_dealer_hand
+    dealer_hand.play
     draw_hands
     draw_bet_options
-  end
-
-  def clear_draw_hands_game_options
-    clear
-    draw_hands
-    draw_game_options
   end
 end
